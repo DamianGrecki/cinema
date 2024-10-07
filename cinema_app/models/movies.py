1951a820830fd504b37af442b4c466e7494f21c9
Damian Grecki <d.grecki@onet.pl>
2024-10-07 17:43:34 +0200

    Migracja na postgresql

--- cinema_app/models/movies.py
+++ cinema_app/models/movies.py
@@ -1,6 +1,12 @@
 # models.py
 from django.db import models
 
+
 class Movies(models.Model):
     title = models.CharField(max_length=100)
     release_year = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    image = models.ImageField(upload_to = 'media/', default='missing_movie_image.jpg')
+
+    def __str__(self):
+        return self.title
--- cinema_app/views/home_view.py
+++ cinema_app/views/home_view.py
@@ -1,8 +1,11 @@
 # cinema_app/home_view.py
+from cinema_app.models.movies import Movies
 from django.shortcuts import render
 
 def home(request):
+    latest_movies = Movies.objects.order_by('-created_at')[:6]
     context = {
-        'text': 'Witaj na stronie głównej!'
+        'text': 'Witaj na stronie głównej!',
+        'latest_movies': latest_movies
     }
     return render(request, 'home.html', context)
